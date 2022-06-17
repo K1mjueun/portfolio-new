a2735183fe723c443671b1a09f44c99f950ab67d
K1mjueun <jueun4779@gmail.com>
2022-06-17 17:49:05 +0900

    마지막 페이지 수정 완료

--- js/module/contact-page.js
+++ js/module/contact-page.js
@@ -3,13 +3,13 @@ const contactPage = document.querySelector("#contact-page");
 contactPage.style.display ="none";
 console.log(contactPage);
 
-console.log(contactStamp1);
-console.log(contactStamp2);
-console.log(contactStamp3);
+// console.log(contactStamp1);
+// console.log(contactStamp2);
+// console.log(contactStamp3);
 
 displayChange(contactStamp1,wpWindie,contactPage);
 displayChange(contactStamp2,wpBubbleroom,contactPage);
 displayChange(contactStamp3,wpToyproject,contactPage);
 
-const goFirstPage = document.querySelector("#note-board-2 > h5");
+const goFirstPage = document.querySelector("#contact-page > h5");
 displayChange(goFirstPage,contactPage,startPage1);
