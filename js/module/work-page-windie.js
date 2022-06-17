f7e1debf82d1a77e182558195dbd4dbfe6bf75fe
K1mjueun <jueun4779@gmail.com>
2022-06-17 14:22:13 +0900

    윈디 작업 소개 페이지 수정중

--- js/module/work-page-windie.js
+++ js/module/work-page-windie.js
@@ -1,6 +1,6 @@
 // work-page-windie
 const wpWindie = document.querySelector("#work-page-windie");
-const backBtn1 = document.querySelector("#work-page-windie>#note-board>.arrow-sticker");
+const backBtn1 = document.querySelector("#work-page-windie>header>.back>.arrow");
 
 displayChange(windie,mainPage1,wpWindie);
 displayChange(backBtn1,wpWindie,mainPage1);
