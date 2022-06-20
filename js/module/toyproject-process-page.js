177b8572eae87941357a948ad13d3f101764c183
K1mjueun <jueun4779@gmail.com>
2022-06-20 13:06:29 +0900

    토이프로젝트 기획 과정 페이지 수정

--- js/module/toyproject-process-page.js
+++ js/module/toyproject-process-page.js
@@ -2,9 +2,9 @@
 const toyprojectProPage = document.querySelector('#toyproject-process-page');
 displayChange(toypjWorkProBtns.children[0],wpToyproject,toyprojectProPage);
 
-const toypjTopBtn = document.querySelector("#toyproject-process-page > #toyproject-process-page-5 > #note-board > .top-sticker");
+const toypjTopBtn = document.querySelector("#toyproject-process-page > #process-page-5 > #process-board > .top-sticker");
 
-const toypjProbackBtn = document.querySelector("#toyproject-process-page > #toyproject-process-page-1 > #note-board > .process-arrow-sticker");
+const toypjProbackBtn = document.querySelector("#toyproject-process-page > #process-page-1 > header > .back");
 displayChange(toypjProbackBtn,toyprojectProPage,wpToyproject);
 // 뒤로가기
 
