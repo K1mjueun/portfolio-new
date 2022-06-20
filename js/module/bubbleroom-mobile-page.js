fcd522d73d0b097cd0b788d7d5987bfa6da59781
K1mjueun <jueun4779@gmail.com>
2022-06-20 15:48:48 +0900

    뭐가 문제인건가

--- js/module/bubbleroom-mobile-page.js
+++ js/module/bubbleroom-mobile-page.js
@@ -2,8 +2,8 @@
 const bubbleroomMbPage = document.querySelector('#bubbleroom-mobile-page');
 displayChange(bubbleroomWorkProBtns.children[2],wpBubbleroom,bubbleroomMbPage);
 
-const bubbleMbbackBtn = document.querySelector("#bubbleroom-mobile-page-1 >#note-board> .mobile-arrow-sticker");
-const bubbleMbTopBtn = document.querySelector("#bubbleroom-mobile-page-2 >#note-board>.mobile-top-sticker");
+const bubbleMbbackBtn = document.querySelector("#bubbleroom-mobile-page > .back");
+const bubbleMbTopBtn = document.querySelector("#bubbleroom-mobile-page > .top-sticker");
 
 displayChange(bubbleMbbackBtn,bubbleroomMbPage,wpBubbleroom);
 // 뒤로가기
--- js/module/windie-mobile-page.js
+++ js/module/windie-mobile-page.js
@@ -2,8 +2,8 @@
 const windieMbPage = document.querySelector('#windie-mobile-page');
 displayChange(windieWorkProBtns.children[1],wpWindie,windieMbPage);
 
-const windieMbbackBtn = document.querySelector("#windie-mobile-page-1 >#note-board> .mobile-arrow-sticker");
-const windieMbTopBtn = document.querySelector("#windie-mobile-page-2 >#note-board>.mobile-top-sticker");
+const windieMbbackBtn = document.querySelector("#windie-mobile-page > .back");
+const windieMbTopBtn = document.querySelector("#windie-mobile-page > .top-sticker");
 
 displayChange(windieMbbackBtn,windieMbPage,wpWindie);
 // 뒤로가기
