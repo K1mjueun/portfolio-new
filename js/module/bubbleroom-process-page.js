5db34f580da7fd7842a950b672b90a5ba406a153
K1mjueun <jueun4779@gmail.com>
2022-06-20 12:57:46 +0900

    중복 효과 삭제, 버블룸 기획 과정 페이지 수정

--- js/module/bubbleroom-process-page.js
+++ js/module/bubbleroom-process-page.js
@@ -2,9 +2,9 @@
 const bubbleroomProPage = document.querySelector('#bubbleroom-process-page');
 displayChange(bubbleroomWorkProBtns.children[0],wpBubbleroom,bubbleroomProPage);
 
-const bubbleroomTopBtn = document.querySelector("#bubbleroom-process-page > #bubbleroom-process-page-5 > #note-board > .top-sticker");
+const bubbleroomTopBtn = document.querySelector("#bubbleroom-process-page > #process-page-5 > #process-board > .top-sticker");
 
-const bubbleroomProbackBtn = document.querySelector("#bubbleroom-process-page > #bubbleroom-process-page-1 > #note-board > .process-arrow-sticker");
+const bubbleroomProbackBtn = document.querySelector("#bubbleroom-process-page > #process-page-1 > header > .back");
 displayChange(bubbleroomProbackBtn,bubbleroomProPage,wpBubbleroom);
 // 뒤로가기
 
--- js/module/windie-process-page.js
+++ js/module/windie-process-page.js
@@ -2,9 +2,9 @@
 const windieProPage = document.querySelector('#windie-process-page');
 displayChange(windieWorkProBtns.children[0],wpWindie,windieProPage);
 
-const windieTopBtn = document.querySelector("#windie-process-page > #windie-process-page-5 > #process-board > .top-sticker");
+const windieTopBtn = document.querySelector("#windie-process-page > #process-page-5 > #process-board > .top-sticker");
 
-const windieProbackBtn = document.querySelector("#windie-process-page > #windie-process-page-1 > header > .back");
+const windieProbackBtn = document.querySelector("#windie-process-page > #process-page-1 > header > .back");
 displayChange(windieProbackBtn,windieProPage,wpWindie);
 // 뒤로가기
 
