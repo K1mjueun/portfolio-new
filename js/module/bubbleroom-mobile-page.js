c18c0d2dcc6f583126be7112d69c83f8f314ce99
K1mjueun <jueun4779@gmail.com>
2022-06-21 11:39:22 +0900

    작업물 작동 페이지 수정중, 모바일 화면이 문제

--- js/module/bubbleroom-mobile-page.js
+++ js/module/bubbleroom-mobile-page.js
@@ -2,8 +2,8 @@
 const bubbleroomMbPage = document.querySelector('#bubbleroom-mobile-page');
 displayChange(bubbleroomWorkProBtns.children[2],wpBubbleroom,bubbleroomMbPage);
 
-const bubbleMbbackBtn = document.querySelector("#bubbleroom-mobile-page > .back");
-const bubbleMbTopBtn = document.querySelector("#bubbleroom-mobile-page > .top-sticker");
+const bubbleMbbackBtn = document.querySelector("#bubbleroom-mobile-page > .app-back");
+const bubbleMbTopBtn = document.querySelector("#bubbleroom-mobile-page > .app-top");
 
 displayChange(bubbleMbbackBtn,bubbleroomMbPage,wpBubbleroom);
 // 뒤로가기
--- js/module/bubbleroom-tablet-page.js
+++ js/module/bubbleroom-tablet-page.js
@@ -2,8 +2,8 @@
 const bubbleroomTbPage = document.querySelector('#bubbleroom-tablet-page');
 displayChange(bubbleroomWorkProBtns.children[3],wpBubbleroom,bubbleroomTbPage);
 
-const bubbleTbbackBtn = document.querySelector("#bubbleroom-tablet-page >#note-board> .tablet-arrow-sticker");
-const bubbleTbTopBtn = document.querySelector("#bubbleroom-tablet-page >#note-board>.tablet-top-sticker");
+const bubbleTbbackBtn = document.querySelector("#bubbleroom-tablet-page > .app-back");
+// const bubbleTbTopBtn = document.querySelector("#bubbleroom-tablet-page > .app-top");
 
 displayChange(bubbleTbbackBtn,bubbleroomTbPage,wpBubbleroom);
 // 뒤로가기
