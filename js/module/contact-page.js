909e61dd43928e3b337bcecec9764469dca92ea8
K1mjueun <jueun4779@gmail.com>
2022-06-24 09:58:43 +0900

    마지막 페이지 인터랙션 추가

--- js/module/contact-page.js
+++ js/module/contact-page.js
@@ -13,3 +13,41 @@ displayChange(contactStamp3,wpToyproject,contactPage);
 
 const goFirstPage = document.querySelector("#contact-page > h5");
 displayChange(goFirstPage,contactPage,startPage1);
+
+let banchak = [
+  {opacity: 1},
+  {opacity: 0.1},
+  {opacity: 1},
+];
+
+  let banchakTime = {
+    duration: 4000,
+    iterations: Infinity
+  };
+
+const decoSticker = document.querySelector("#contact-page > .deco-sticker");
+if(contactPage.style.opacity = 1) {
+    decoSticker.animate(banchak,banchakTime);
+};
+
+const emailAddress = document.querySelector(".personal-1 > h3");
+const githubAddress = document.querySelector(".personal-2 > h4");
+
+let addressMoving = [
+  { transform: 'rotate(-1deg)'},
+  { transform: 'rotate(1deg)'},
+  { transform: 'rotate(0deg)'}
+];
+  let addressMovingTime = {
+    duration: 700,
+    iterations: 1
+  };
+
+function mouseOver(eventtarget) {
+  eventtarget.addEventListener('mouseover', function() {
+    eventtarget.animate(addressMoving,addressMovingTime);
+  });
+};
+
+mouseOver(emailAddress);
+mouseOver(githubAddress);
